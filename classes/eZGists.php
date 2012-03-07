4e8c54a83b01ce20bd490fe7e92576faf9d247ea
Damien MARTIN <damien@martin-damien.fr>
2012-03-07 22:27:37 +0100

    Get the list of all the gists for a given username.

--- classes/eZGists.php
+++ classes/eZGists.php
@@ -3,21 +3,46 @@
     class eZGists
     {
 
+        // STATIC METHODS ///////////////////////////////////////////
+
+        /**
+         * Get all the gists for $username.
+         * @param $username If null, it will use the username written in ezgithub.ini
+         * @return An array of Gists
+         */
         public static function getGists( $username = null )
         {
+
+            $ini = eZINI::instance( 'ezgithub.ini' );
+
+            // Select correct user
             if ( $username == null )
             {
-                $ini = eZINI::instance( 'ezgithub.ini' );
-                $username = $ini->value( 'Profile', 'username' );
+                $username = $ini->variable( 'Profile', 'username' );
             }
-            return "Temporary Result";
+
+            // Construct the URL to call
+            $api_url = $ini->variable( 'API', 'url' )."/users/$username/gists";
+
+            // Get datas
+            $rsc = curl_init();
+            curl_setopt( $rsc, CURLOPT_URL, $api_url );
+            curl_setopt( $rsc, CURLOPT_RETURNTRANSFER, true );
+            $result = curl_exec( $rsc );
+            curl_close( $rsc );
+
+            // Process datas
+            $datas = json_decode( $result );
+
+            return $datas;
+
         }
 
         // FETCHS ///////////////////////////////////////////////////
         
         public static function fetchGetGists( $username = null )
         {
-            return array( 'result' => $this->getGists( $username ) );
+            return array( 'result' => eZGists::getGists( $username ) );
         }
 
     }
